a1680f1a8e5c4e216395ae1af3912f1b27456764
Adithya Ajay Somkuwar <70945483+ADITHYAAAS@users.noreply.github.com>
2020-09-10 14:54:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var movingrect, fixedrect;
+
+function setup() {
+  createCanvas(800,400);
+  fixedrect = createSprite(200, 200, 50, 100);
+  movingrect = createSprite(400,200,80,40);
+
+  fixedrect.shapeColor = "red";
+  movingrect.shapeColor = "red";
+}
+
+function draw() {
+  background(0);  
+
+movingrect.x = World.mouseX;
+movingrect.y = World.mouseY;
+
+if(movingrect.x - fixedrect.x < movingrect.width/2 + fixedrect.width/2 && 
+  fixedrect.x - movingrect.x < movingrect.width/2 + fixedrect.width/2 &&
+  movingrect.y - fixedrect.y < movingrect.height/2 + fixedrect.height/2 && 
+  fixedrect.y - movingrect.y < movingrect.height/2 + fixedrect.height/2)
+{
+  fixedrect.shapeColor = "yellow";
+  movingrect.shapeColor = "yellow";
+}
+
+else{
+  fixedrect.shapeColor = "red";
+  movingrect.shapeColor = "red";
+}
+
+  drawSprites();
+}
